4b6fe83797b74fa6aaabace5b8961505d08ae2f8
dioppolo <diego.ioppolo2006@gmail.com>
2026-04-01 09:35:23 +0200

    ex4

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -11,7 +11,7 @@ def get_inventory(d, arg):
 
 if __name__ == "__main__":
     print("=== Inventory System Analysis ===\n")
-    d = {}
+    d: dict[str, str] = {}
     for arg in sys.argv[1:]:
         try:
             d = get_inventory(d, arg)
@@ -19,35 +19,40 @@ if __name__ == "__main__":
             print(f"Error - invalid parameter: '{arg}'")
         except Exception:
             print(f"Reduntant item '{arg}' - discarding")
-    new_d = {}
+    new_d: dict[str, int] = {}
     for k in d:
         try:
             new_d[k] = int(d[k])
         except ValueError:
-            print(f"Quantity error for '{k}': invalid literal fot int(): '{d[k]}'")
-    d = new_d
-    print(f"Got inventory: {d}")
-    items = d.keys()
+            print(
+                f"Quantity error for '{k}': invalid"
+                f"literal fot int(): '{d[k]}'"
+                )
+    d_int: dict[str, int] = new_d
+    print(f"Got inventory: {d_int}")
+    items = d_int.keys()
     print(f"Item list: {items}")
     i = 0
-    for count in d:
+    for count in d_int:
         i += 1
-    num_item = sum(d.values())
+    num_item = sum(d_int.values())
     print(f"Total quantity of the {i} items: {num_item}")
-    for item in d:
-        quantity = round(((d[item] * 100) / num_item))
+    for item in d_int:
+        quantity = round(((d_int[item] * 100) / num_item))
         print(f"Item {item} represents {quantity}%")
-    biggest = None
-    for num in d:
-        if biggest is None or d[num] > biggest:
-            biggest = d[num]
+    biggest: int | None = None
+    biggest_name: str | None = None
+    for num in d_int:
+        if biggest is None or d_int[num] > biggest:
+            biggest = d_int[num]
             biggest_name = num
     print(f"Item most abundant: {biggest_name} with quantity {biggest}")
-    lowest = None
-    for num in d:
-        if lowest is None or d[num] < lowest:
-            lowest = d[num]
+    lowest: int | None = None
+    lowest_name: str | None = None
+    for num in d_int:
+        if lowest is None or d_int[num] < lowest:
+            lowest = d_int[num]
             lowest_name = num
     print(f"Item least abundant: {lowest_name} with quantity {lowest}")
-    d.update({'magic_item': 1})
-    print(f"Updated inventory: {d}")
+    d_int.update({'magic_item': 1})
+    print(f"Updated inventory: {d_int}")
