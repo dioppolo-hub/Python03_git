4be8b8d95772d5905d7981862dd47ac70687c954
dioppolo <dioppolo@student.42firenze.com>
2026-04-01 19:50:56 +0200

    ex5 no flake8 no mypy

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -10,6 +10,14 @@ def gen_event(Player_list, Action_list) -> typing.Generator[tuple, None, None]:
         yield (random.choice(Player_list), random.choice(Action_list))
 
 
+def consume_event(lst) -> typing.Generator[list, None, None]:
+    while True:
+        size = len(lst)
+        if size == 0:
+            break
+        temp = random.choice(lst)
+        yield temp
+
 
 if __name__ == "__main__":
     Player_list = ["Bob", "Dylan", "Alice", "Frank", "Sofia", "Deb"]
@@ -21,3 +29,19 @@ if __name__ == "__main__":
         event = next(event_gen)
         print(f"Event {i}: Player {event[0]} did action {event[1]}")
         i += 1
+    lst = []
+    x = 10
+    while x > 0:
+        event = next(event_gen)
+        lst.append(event)
+        x -= 1
+    print(f"Built list of 10 events: {lst}")
+    lst_gen = consume_event(lst)
+    while True:
+        size = len(lst)
+        if size == 0:
+            break
+        lst_tuple = next(lst_gen)
+        print(f"Got event from list: {lst_tuple}")
+        lst.remove(lst_tuple)
+        print(f"Remains in list: {lst}")
